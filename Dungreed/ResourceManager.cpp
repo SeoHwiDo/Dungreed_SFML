62b1342291158d1944e7a0db2f871cd632cab04c
user <user@DESKTOP-CTA5QIP>
2026-08-04 15:52:56 +0900

    [init]최초 업로드

---
+++ Dungreed/ResourceManager.cpp
@@ -0,0 +1,71 @@
+﻿#include "ResourceManager.h"
+
+
+bool ResourceManager::checkFileExists(const std::string& path) const{
+	if(!fs::exists(path)||!fs::is_directory(path)){
+		//폴더가 없거나 폴더가 아니면 에러
+		std::cerr << "폴더 명이 잘못되었습니다. 경로를 확인해주세요: " << path << std::endl;
+		return false;
+	}
+	return true;
+}
+
+void ResourceManager::loadActorImages(const std::string& path, const std::string& actionName, ActorImage& actorImage)
+{
+	if (checkFileExists(path))return;
+	std::vector<std::string>filePath;
+	//폴더 내 파일 경로 추출
+	for (const auto& entry : fs::directory_iterator(path)) {
+		if (fs::is_regular_file(entry.path())) {
+			filePath.push_back(entry.path().string());
+		}
+	}
+	std::sort(filePath.begin(), filePath.end());//파일 이름 순으로 정렬
+
+	for (const auto& file : filePath) {
+		//확장자
+		std::string ext = fs::path(path).extension().string();
+		if(ext==".png"||ext==".jpg"||ext==".jpeg"||ext==".bmp"||ext==".tga"||ext==".gif"){
+			//이미지 로드
+			auto texture = std::make_unique<sf::Texture>();
+			if (!texture->loadFromFile(file)) {
+				//이미지 로드 실패시 에러 메시지 출력
+				std::cerr << "이미지 로드 실패: " << file << std::endl;
+				continue;
+			}
+			//이미지 로드 성공시 textures에 추가
+			actorImage.textures[actionName].push_back(std::move(texture));
+		}
+	}
+}
+
+void ResourceManager::loadImages(){
+	//플레이어 이미지 로드
+	if(checkFileExists(PLAYER_PATH)) {
+		for (const auto& entry : fs::directory_iterator(PLAYER_PATH)) {
+			//바로 행동 폴더로이동
+			if(fs::is_directory(entry.path())){
+				//폴더 이름을 액션 이름으로 사용
+				std::string actionName = entry.path().filename().string();
+				loadActorImages(entry.path().string(), actionName, playerImages);
+			}
+		}
+	}
+	//몬스터 이미지 로드
+	if (checkFileExists(MONSTER_PATH)) {
+		for (const auto& monster : fs::directory_iterator(MONSTER_PATH)) {
+			//하위의 몬스터종류별 폴더로 이동
+			if (fs::is_directory(monster.path())) {
+				for (const auto& entry : fs::directory_iterator(monster.path())) {
+					if(fs::is_directory(entry.path())) {
+						//폴더 이름을 액션 이름으로 사용
+						std::string actionName = entry.path().filename().string();
+						loadActorImages(entry.path().string(), actionName, enemyImages[monster.path().filename().string()]);
+					}
+				}
+			}
+		}
+	}
+	//타일맵 이미지 로드
+
+}
---
+++ Dungreed/ResourceManager.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include<string>
+#include<memory>
+#include<stdexcept>
+#include <filesystem> 
+namespace fs = std::filesystem;
+#include<unordered_map>
+#include<iostream>
+#include<SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
+const std::string RESOURCE_PATH = "resources/";
+const std::string IMAGE_PATH = RESOURCE_PATH + "images/";
+const std::string PLAYER_PATH = IMAGE_PATH + "player/";
+const std::string MONSTER_PATH = IMAGE_PATH + "monster/";
+const std::string BOSS_PATH = IMAGE_PATH + "boss/";
+const std::string TILEMAP_PATH = IMAGE_PATH + "TileMaps/";
+const std::string Background_PATH = IMAGE_PATH + "Backgrounds/";
+
+
+class ResourceManager{
+private:
+	struct ActorImage {
+		std::unordered_map<std::string, std::vector<std::unique_ptr<sf::Texture>>> textures;
+	};
+	ActorImage playerImages;
+	std::unordered_map<std::string, ActorImage> enemyImages;
+	std::unordered_map<std::string, std::unique_ptr<sf::Texture>> TownTileMapImages;
+	std::unordered_map<std::string, std::unique_ptr<sf::Texture>> DungeonTileMapImages;
+	bool checkFileExists(const std::string& path) const;
+	void loadActorImages(const std::string& path,const std::string&actionName, ActorImage& actorImage);
+public:
+	// Resource class to manage resources in a game or application
+	void loadImages(); // Load resources from files
+	const sf::Texture& getPlayerImage(const std::string& action) const;
+	const sf::Texture& getEnemyImage(const std::string& name, const std::string& action) const;
+	const sf::Texture& getDungeonTileMapImage(const std::string& name, const std::string& type) const;
+	const sf::Texture& getTownTileMapImage(const std::string& name, const std::string& type) const;
+};
+
