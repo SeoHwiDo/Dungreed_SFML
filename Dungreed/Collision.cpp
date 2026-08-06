7cdb7114812f8a39a6b50f75386a943dfb5978d3
user <user@DESKTOP-CTA5QIP>
2026-08-06 10:29:47 +0900

    [add] 객체 피격시 정확한 피격 위치를 반환하는 함수 구현

--- Dungreed/Collision.cpp
+++ Dungreed/Collision.cpp
@@ -1,1 +1,17 @@
 ﻿#include "Collision.h"
+#include<optional>
+std::optional<sf::Vector2f> Collision::checkHit(const sf::FloatRect& attackBox) const {
+    auto intersection = m_hitbox.findIntersection(attackBox);
+
+    if (intersection.has_value()) {
+        sf::FloatRect overlap = intersection.value();
+
+        // 겹친 영역의 정중앙 좌표를 계산하여 반환
+        return sf::Vector2f{
+            overlap.position.x + (overlap.size.x / 2.f),
+            overlap.position.y + (overlap.size.y / 2.f)
+        };
+    }
+
+    return std::nullopt; // 충돌하지 않음
+}
--- Dungreed/Collision.h
+++ Dungreed/Collision.h
@@ -1,14 +1,21 @@
 ﻿#pragma once
-#include "Actor.h"
 
-class collision {
+#include <SFML/Graphics.hpp>
+
+
+class Collision {
 public:
-    // 맵(타일)과의 충돌 여부 및 중력/물리 처리 적용
-    static bool mapCheck(Actor* actor);
+    // 생성 시 이 콜리전 객체의 주인을 등록합니다.
+    Collision()=default;
+    ~Collision() = default;
 
-    // 공격 판정 확인
-    static bool attackCheck(Actor* actor);
+    // 매 프레임 스프라이트 위치에 맞게 피격 박스(Hitbox)를 갱신합니다.
+    inline void updateHitbox(const sf::FloatRect& actorBounds) { m_hitbox = actorBounds;}
+    //피격 위치 확인 후 정확한 피격지점 반환, 없을 경우 nullopt반환
+    std::optional<sf::Vector2f> checkHit(const sf::FloatRect& attackBox) const;
 
-    // 피격 판정 확인 (피격당한 대상 객체 반환)
-    static Actor* hitCheck();
+    inline const sf::FloatRect& getHitbox() const { return m_hitbox; }
+
+private:
+    sf::FloatRect m_hitbox;
 };
